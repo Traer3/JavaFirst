87dd23843f349ed64b25540b61d95e09a4bfe258
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2021-12-12 11:39:32 +0200

    Update HM6.java

--- homework/HM6.java
+++ homework/HM6.java
@@ -1,35 +1,48 @@
 package com.hillel.homework;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HM6 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        String airFighter = "Jet,model:";
-        String model = "F-22 Raptor,";
-        String empty = " ";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String Typ1 = printAirplaneInfo(airFighter, model);
-        System.out.println(Typ1);
+        System.out.println();
+        System.out.println("Введите название истребителя");
+        String airFighterName = reader.readLine();
+        System.out.println("Введите тип истребителя");
+        String airFighterType = reader.readLine();
+        System.out.println("Введите тип эконом самолета");
+        String aircraftName = reader.readLine();
+        System.out.println("Введите модель эконом самолета");
+        String aircraftType = reader.readLine();
+        System.out.println("Введите количество мест эконом класса");
+        String economyClassSeats = reader.readLine();
+        System.out.println("Введите тип бизнес самолета");
+        String businessAircraftName = reader.readLine();
+        System.out.println("Введите модель бизнес самолета");
+        String businessAircraftType = reader.readLine();
+        System.out.println("Введите количество мест эконом класса");
+        String businessEconomyClassSeats = reader.readLine();
+        System.out.println("Введите количество мест бизнес класса");
+        String businessClassSeats = reader.readLine();
 
-        printAirplaneInfo();
-
-        String Typ2 = printAirplaneInfo(empty);
-        System.out.println(Typ2);
-
+        printAirplaneInfo(airFighterName,airFighterType);
+        printAirplaneInfo(aircraftName,aircraftType,economyClassSeats);
+        printAirplaneInfo(businessAircraftName,businessAircraftType,businessEconomyClassSeats,businessClassSeats);
 
     }
-
-    public static String printAirplaneInfo(String airFighter, String model) {
-        return (airFighter + model);
+    public static void printAirplaneInfo(String airFighterName,String airFighterType){
+        System.out.println("Тип самалета: " + airFighterName + ",модель: " + airFighterType);
     }
-
-    public static void printAirplaneInfo() {
-        System.out.println("Airliner, модель: Aerobus 737, кол. пасажиров эконом класса: 120");
-
+    static void printAirplaneInfo(String aircraftName,String aircraftType,String economyClassSeats){
+        System.out.println("Тип самалета: " + aircraftName + ",модель: " + aircraftType + " кол. пасажиров эконом класса: " + economyClassSeats );
     }
-    public static String printAirplaneInfo(String empty) {
-        System.out.println("Airliner, модель: Boeing 777, кол. пасажиров эконом класса: 180, пассажиров бизне скласса: 28");
-
-        return empty;
+    static void printAirplaneInfo(String businessAircraftName,String businessAircraftType,String businessEconomyClassSeats, String businessClassSeats ){
+        System.out.println("Тип самалета: " + businessAircraftName + ",модель: " + businessAircraftType + " кол. пасажиров эконом класса: " + businessEconomyClassSeats + ", пассажиров бизне скласса:" + businessClassSeats );
     }
 
 }
