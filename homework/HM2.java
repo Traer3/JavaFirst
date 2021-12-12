eafffc6d3e043ec0eef1bb7da1f039fb9dc48c48
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2021-12-12 19:54:06 +0200

    Update HM2.java

--- homework/HM2.java
+++ homework/HM2.java
@@ -1,104 +1,82 @@
 package com.hillel.homework;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HM2 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println();
-        System.out.print("My favorite places in Odessa ");
+        System.out.print("Мои любимые места в одессе  ");
         char ch3 = '\u263A';
         System.out.println(ch3);
-
-
-        //System.out.println("#1;#2;#3.....#8"); would be too easy =)
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите числа a и b :");
-
-        byte a = sc.nextByte();
-
         System.out.println();
+        System.out.println("Выбери случайное число от 1 - 8: ");
 
-        byte myByte1 = 1;
-        System.out.println("#" + myByte1);
+        int number = Integer.parseInt(reader.readLine());
+        System.out.println("Кординаты №: " + number);
 
-        System.out.print("latitude : ");
-        System.out.println("46.58962604053726");
-        System.out.print("longitude : ");
-        System.out.println("30.795835136752896");
+        printPlace(number);
 
-        System.out.println();
+    }
 
-        byte myByte2 = 2;
-        System.out.println("#" + myByte2);
-
-        System.out.print("latitude : ");
-        System.out.println("46.48393665026677");
-        System.out.print("longitude : ");
-        System.out.println("30.737154831200446");
-
-        System.out.println();
-
-        byte myByte3 = 3;
-        System.out.println("#" + myByte3);
-
-        System.out.print("latitude : ");
-        System.out.println("46.475534198814245");
-        System.out.print("longitude : ");
-        System.out.println("30.709326353158957");
-
-        System.out.println();
-
-        byte myByte4 = 4;
-        System.out.println("#" + myByte4);
-
-        System.out.print("latitude : ");
-        System.out.println("46.46898367432307");
-        System.out.print("longitude : ");
-        System.out.println("30.742029039125832");
-
-        System.out.println();
-
-        byte myByte5 = 5;
-        System.out.println("#" + myByte5);
-
-        System.out.print("latitude : ");
-        System.out.println("46.460147093794056");
-        System.out.print("longitude : ");
-        System.out.println("30.749543826476486");
-
-        System.out.println();
-
-        byte myByte6 = 6;
-        System.out.println("#" + myByte6);
-
-        System.out.print("latitude : ");
-        System.out.println("46.43760446601472");
-        System.out.print("longitude : ");
-        System.out.println("30.74637102651043");
-
-        System.out.println();
-
-        byte myByte7 = 7;
-        System.out.println("#" + myByte7);
-
-        System.out.print("latitude : ");
-        System.out.println("46.430418911355666");
-        System.out.print("longitude : ");
-        System.out.println("30.729014019009497");
-
-        System.out.println();
-
-        byte myByte8 = 8;
-        System.out.println("#" + myByte8);
-
-        System.out.print("latitude : ");
-        System.out.println("46.399042093482215");
-        System.out.print("longitude : ");
-        System.out.println("30.7222088039146");
-
-
-
-
+    private static void printPlace(int number) {
+        switch (number) {
+            case 1:
+                System.out.print("latitude : ");
+                System.out.println("46.58962604053726");
+                System.out.print("longitude : ");
+                System.out.println("30.795835136752896");
+                break;
+            case 2:
+                System.out.print("latitude : ");
+                System.out.println("46.48393665026677");
+                System.out.print("longitude : ");
+                System.out.println("30.737154831200446");
+                break;
+            case 3:
+                System.out.print("latitude : ");
+                System.out.println("46.475534198814245");
+                System.out.print("longitude : ");
+                System.out.println("30.709326353158957");
+                break;
+            case 4:
+                System.out.print("latitude : ");
+                System.out.println("46.46898367432307");
+                System.out.print("longitude : ");
+                System.out.println("30.742029039125832");
+                break;
+            case 5:
+                System.out.print("latitude : ");
+                System.out.println("46.460147093794056");
+                System.out.print("longitude : ");
+                System.out.println("30.749543826476486");
+                break;
+            case 6:
+                System.out.print("latitude : ");
+                System.out.println("46.43760446601472");
+                System.out.print("longitude : ");
+                System.out.println("30.74637102651043");
+                break;
+            case 7:
+                System.out.print("latitude : ");
+                System.out.println("46.430418911355666");
+                System.out.print("longitude : ");
+                System.out.println("30.729014019009497");
+                break;
+            case 8:
+                System.out.print("latitude : ");
+                System.out.println("46.399042093482215");
+                System.out.print("longitude : ");
+                System.out.println("30.7222088039146");
+                break;
+            default:
+                System.out.println("Неизвестное место");
+                break;
+        }
     }
 }
