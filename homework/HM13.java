3cfbb26472098d71fec3ec36f4eb37aca2dff9a8
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2021-12-28 17:08:14 +0200

    Update HM13.java

--- homework/HM13.java
+++ homework/HM13.java
@@ -7,30 +7,35 @@ import java.util.Random;
 
 public class HM13 {
     public static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
         Random generator = new Random();
         int mysteryNumber = generator.nextInt(0, 10);
 
 
-        String randomNumber=String.valueOf(mysteryNumber);
+        String randomNumber = String.valueOf(mysteryNumber);
         System.out.println("Введите случайное число в диапазоне от 1 до 10");
         String input = READER.readLine();
+        GuessNumber(randomNumber, input);
 
 
+    }
 
-        while(!input.equalsIgnoreCase(randomNumber)) {
+    static void GuessNumber(String randomNumber, String input) throws IOException {
+        while (!input.equalsIgnoreCase(randomNumber)) {
 
-            Integer random=Integer.valueOf(randomNumber);
-            Integer number= Integer.valueOf(input);
-            if (random > number){
+            Integer random = Integer.valueOf(randomNumber);
+            Integer number = Integer.valueOf(input);
+            if (random > number) {
                 System.out.println("Мало");
             }
-            if (random < number){
+            if (random < number) {
                 System.out.println("Много");
             }
 
             System.out.println("Вы ввели : " + input);
             input = READER.readLine();
         }
+
     }
 }
