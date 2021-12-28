d8604e305f99ca64ef866c2ac7723791ac1f5932
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2021-12-28 16:32:17 +0200

    Update HM11.java

--- homework/HM11.java
+++ homework/HM11.java
@@ -5,26 +5,32 @@ import java.util.Random;
 public class HM11 {
     public static void main(String[] args) {
 
+        RandomNumbers();
+
+
+    }
+    static void RandomNumbers(){
         Random generator = new Random();
-        int min = generator.nextInt(0, 10);
-        int max = min;
-        int mid = min;
+        int lowNumber = generator.nextInt(1, 10);
+        int highNumber = lowNumber;
+        int middleNumber = lowNumber;
         int i;
         for (i = 0; i < 3; i++) {
-            int randomInt = generator.nextInt(0, 10);
-            if (randomInt < min) {
-                min = randomInt;
+            int randomInt = generator.nextInt(1, 10);
+            if (randomInt < lowNumber) {
+                lowNumber = randomInt;
             }
-            if (randomInt > max) {
-                max = randomInt;
+            if (randomInt > highNumber) {
+                highNumber = randomInt;
             }
-            if (mid < max && mid > min) {
+            if (middleNumber < highNumber && middleNumber > lowNumber) {
 
             }
 
         }
-        System.out.println("min: " + min + " mid: " + mid + " max: " + max);
-
+        System.out.println("Минимальное число: " + lowNumber + " Среднее число: " + middleNumber + " Максимальное число : " + highNumber);
+        System.out.println();
+        System.out.println("Минимальное число: " + lowNumber);
 
     }
 
