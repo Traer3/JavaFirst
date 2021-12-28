1095a9f36513210a4d6d1139f1459e712c50fa4b
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2021-12-28 16:40:05 +0200

    Update HM11.java

--- homework/HM11.java
+++ homework/HM11.java
@@ -4,16 +4,17 @@ import java.util.Random;
 
 public class HM11 {
     public static void main(String[] args) {
-
-        RandomNumbers();
-
-
-    }
-    static void RandomNumbers(){
         Random generator = new Random();
         int lowNumber = generator.nextInt(1, 10);
         int highNumber = lowNumber;
         int middleNumber = lowNumber;
+
+
+        RandomNumbers(lowNumber,highNumber,middleNumber,generator);
+
+
+    }
+    static void RandomNumbers(int lowNumber,int highNumber,int middleNumber,Random generator){
         int i;
         for (i = 0; i < 3; i++) {
             int randomInt = generator.nextInt(1, 10);
