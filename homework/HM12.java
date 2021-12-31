883d984eed1e8a41949d5b5d69ba85eb5fe44669
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2021-12-31 15:48:22 +0200

    Update HM12.java

--- homework/HM12.java
+++ homework/HM12.java
@@ -4,51 +4,15 @@ package com.hillel.homework;
 public class HM12 {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 101; i++) {
+        for (int i = 0; i < 100; i++) {
 
             if (i % 10 == 4 || i % 10 == 9) {
                 i++;
             }
-            if (i == 40 )
-                i++;
-            if (i == 41 )
-                i++;
-            if (i == 42 )
-                i++;
-            if (i == 43 )
-                i++;
-            if (i == 44 )
-                i++;
-            if (i == 45 )
-                i++;
-            if (i == 46 )
-                i++;
-            if (i == 47 )
-                i++;
-            if (i == 48 )
-                i++;
-            if (i == 49 )
-                i++;
-            if (i == 90 )
-                i++;
-            if (i == 91 )
-                i++;
-            if (i == 92 )
-                i++;
-            if (i == 93 )
-                i++;
-            if (i == 94 )
-                i++;
-            if (i == 95 )
-                i++;
-            if (i == 96 )
-                i++;
-            if (i == 97 )
-                i++;
-            if (i == 98 )
-                i++;
-            if (i == 99 )
-                i++;
+            if (i/10 == 4 || i/10 == 9 ){
+                continue;
+
+            }
 
 
             System.out.println(i);
