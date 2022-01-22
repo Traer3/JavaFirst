2757902af2619c66eee84cba8ba743a7887fa2b5
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2022-01-22 14:55:57 +0200

    Update HM18.java

--- homework/HM18.java
+++ homework/HM18.java
@@ -1,50 +1,78 @@
 package com.hillel.homework;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class HM18 {
+    static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) {
 
-        int[] first = {5, 6, 7, 8, 9, 1, 2, 3,};
-        int[] second = {7, 5, 3, 8, 7, 8, 3, 0};
+        System.out.println("Введите первый масив чисел");
+        System.out.println();
+        int[] first = arrayLength();
+        System.out.println("Введите второй масив чисел");
+        System.out.println();
+        int[] second = arrayLength();
+
+        arrayFill(first);
+        arrayFill(second);
 
 
-        for (int i = 0; i < first.length; i++)
-            for (int j = 0; j < first.length; j++) {
-                if (first[j] > first[i]) {
-                    int temp = first[j];
-                    first[j] = first[i];
-                    first[i] = temp;
-                }
-            }
+        bubbleSort(first);
+        bubbleSort(second);
 
-        for (int i = 0; i < second.length; i++)
-            for (int j = 0; j < second.length; j++) {
-                if (second[j] > second[i]) {
-                    int temp = second[j];
-                    second[j] = second[i];
-                    second[i] = temp;
-                }
-            }
-
-        for (int i = 0; i < first.length; i++) {
-            for (int j = 0; j < second.length; j++) {
-                if(first[i] == second[i]);
-                System.out.println("Элемент = " + i);
-            }
-
-
-
-
-
-        }
-
-
-
-
+        overlap(first, second);
 
         System.out.println(Arrays.toString(first));
         System.out.println(Arrays.toString(second));
     }
 
+    private static int[] bubbleSort(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++)
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[j] > numbers[i]) {
+                    swap(numbers, i, j);
+                }
+            }
+        return numbers;
+    }
+
+    private static void swap(int[] numbers, int i, int j) {
+        int temp = numbers[j];
+        numbers[j] = numbers[i];
+        numbers[i] = temp;
+    }
+
+    private static void overlap(int[] first, int[] second) {
+
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] == second[i]) {
+                System.out.println("совпали элементы " + first[i] + " и " + second[i]);
+            }
+
+        }
+    }
+    private static int[] arrayLength() {
+        try {
+            System.out.println("Сколько чисел вы хотите ввести?  ");
+            int[] numbers = new int[Integer.parseInt(READER.readLine())];
+            return numbers;
+        } catch (Exception e) {
+            System.out.println("Ошибка введите значения еще раз");
+            return arrayLength();
+        }
+    }
+    public static void arrayFill(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Введите значение для  " + (i+1) + " элемента ");
+            try {
+                numbers[i] = Integer.parseInt(READER.readLine());
+            } catch (Exception e) {
+                System.out.println("Ошибка введите значения еще раз");
+                arrayFill(numbers);
+            }
+
+        }
+    }
 }
