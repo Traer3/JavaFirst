336f9422c104f34a74896e40535d12806f4bcfb4
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2022-01-29 19:27:08 +0200

    Add files via upload

---
+++ homework/HM27/Person.java
@@ -0,0 +1,61 @@
+package com.hillel.homework.HM27;
+
+import java.util.Random;
+
+public class Person {
+    String name;
+    String secondName;
+    int age;
+    int weight;
+    int size;
+
+    public Person(String name, String secondName) {
+        this.name = name;
+        this.secondName = secondName;
+    }
+
+    public Person(String name, String secondName, int age) {
+        this.name = name;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public Person(String name, String secondName, int age, int weight, int size) {
+        this.name = name;
+        this.secondName = secondName;
+        this.age = age;
+        this.weight = weight;
+        this.size = size;
+    }
+
+    public static String personGenerator(){
+
+        Random randomNamer = new Random();
+        String[] firstNames = { "Myles", "Eliezer", "Douglas", "Darian", "Brock", "Johnny", "Reed", "Howard", "Cristofer", "Kolten", "Eugene", "Neil", "Jason", "Franklin", "Joe", "Jerry", "Allen", "Samuel", "Dario", "Nelson", "Antonio", "Carsen", "Adan", "Alijah"  };
+        String[] secondNames = { " Lester", " Bryan", " Rojas", " Cameron", " Vega", " Dickson", " Leonard", " Faulkner", " Kelly", " Friedman", "  Kaiser" ,"Hester","Francis","Burke","Flowers","Santos","Farrell","Myers","Key","Maynard","Davidson"};
+
+        String randomName = "";
+
+        for (int i = 0; i < 1; i++) {
+            int firstIndex = randomNamer.nextInt(firstNames.length);
+            int secondIndex = randomNamer.nextInt(secondNames.length);
+
+            randomName = " " + firstNames[firstIndex] + " " + secondNames[secondIndex];
+
+
+        }
+
+        return randomName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", size=" + size +
+                '}';
+    }
+}
---
+++ homework/HM27/PersonGenerator.java
@@ -0,0 +1,18 @@
+package com.hillel.homework.HM27;
+
+public class PersonGenerator {
+    public static void main(String[] args) {
+
+
+        String[] randomPerson = new String[100];
+
+        for (int i = 0; i < randomPerson.length; i++) {
+
+            randomPerson[i] += Person.personGenerator();
+            System.out.println(randomPerson[i]);
+        }
+    }
+
+
+
+}
