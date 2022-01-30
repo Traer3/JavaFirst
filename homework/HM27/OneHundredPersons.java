9997131d363ab01d17997d80510b0b4f59790615
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2022-01-30 12:19:28 +0200

    Update OneHundredPersons.java

--- homework/HM27/OneHundredPersons.java
+++ homework/HM27/OneHundredPersons.java
@@ -6,14 +6,20 @@ public class OneHundredPersons {
 
         String[] randomPerson = new String[100];
 
-
         for (int i = 0; i < randomPerson.length; i++) {
 
-            randomPerson[i] = Person.personGenerator();
+           randomPerson[i] += new Person(Person.nameGenerator(),Person.secondNameGenerator(),Person.personAgeGenerator(),Person.personWeightGenerator(),Person.personSizeGenerator());
 
             System.out.println(randomPerson[i]);
-            System.out.println("");
         }
+
+
+
+
+
+
+
+
     }
 
 
