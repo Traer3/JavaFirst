8c6185558791f300049fb72cbb107da1277c26c9
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2022-01-30 12:19:57 +0200

    Update Person.java

--- homework/HM27/Person.java
+++ homework/HM27/Person.java
@@ -21,6 +21,7 @@ public class Person {
     }
 
     public Person(String name, String secondName, int age, int weight, int size) {
+
         this.name = name;
         this.secondName = secondName;
         this.age = age;
@@ -28,31 +29,43 @@ public class Person {
         this.size = size;
     }
 
-    public static String personGenerator(){
 
+    public static String nameGenerator() {
         Random randomNamer = new Random();
-        String[] firstNames = { "Myles", "Eliezer", "Douglas", "Darian", "Brock", "Johnny", "Reed", "Howard", "Cristofer", "Kolten", "Eugene", "Neil", "Jason", "Franklin", "Joe", "Jerry", "Allen", "Samuel", "Dario", "Nelson", "Antonio", "Carsen", "Adan", "Alijah"  };
-        String[] secondNames = { " Lester", " Bryan", " Rojas", " Cameron", " Vega", " Dickson", " Leonard", " Faulkner", " Kelly", " Friedman", "  Kaiser" ,"Hester","Francis","Burke","Flowers","Santos","Farrell","Myers","Key","Maynard","Davidson"};
 
+        String[] firstNames = {"Myles", "Eliezer", "Douglas", "Darian", "Brock", "Johnny", "Reed", "Howard", "Cristofer", "Kolten", "Eugene", "Neil", "Jason", "Franklin", "Joe", "Jerry", "Allen", "Samuel", "Dario", "Nelson", "Antonio", "Carsen", "Adan", "Alijah"};
         String name = "";
+        int firstIndex = randomNamer.nextInt(firstNames.length);
+        name = firstNames[firstIndex];
+        return name;
+    }
+
+    public static String secondNameGenerator() {
+        Random randomNamer = new Random();
+
+        String[] secondNames = {" Lester", " Bryan", " Rojas", " Cameron", " Vega", " Dickson", " Leonard", " Faulkner", " Kelly", " Friedman", "  Kaiser", "Hester", "Francis", "Burke", "Flowers", "Santos", "Farrell", "Myers", "Key", "Maynard", "Davidson"};
         String secondName = "";
+        int secondIndex = randomNamer.nextInt(secondNames.length);
+        secondName = secondNames[secondIndex];
+        return secondName;
+    }
 
-        for (int i = 0; i < 1; i++) {
-            int firstIndex = randomNamer.nextInt(firstNames.length);
-            int secondIndex = randomNamer.nextInt(secondNames.length);
-
-            name = firstNames[firstIndex];
-            secondName = secondNames[secondIndex];
-
-        }
+    public static int personAgeGenerator() {
         double random = Math.random();
         int age = (int) (10 + random * 60);
+        return age;
+    }
 
+    public static int personWeightGenerator() {
+        double random = Math.random();
         int weight = (int) (45 + random * 150);
+        return weight;
+    }
 
+    public static int personSizeGenerator() {
+        double random = Math.random();
         int size = (int) (150 + random * 50);
-
-        return " Name: " + name+ " Second Name: "+ secondName  + " age: " +age+ " weight: " +weight + " size: " +size;
+        return size;
     }
 
     @Override
