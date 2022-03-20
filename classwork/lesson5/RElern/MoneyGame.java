0d8c0bf47d91d37c3132f7b217d531068db6f54d
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2022-03-20 16:04:07 +0200

    Add files via upload

---
+++ classwork/lesson5/RElern/MoneyGame.java
@@ -0,0 +1,54 @@
+package com.hillel.classwork.lesson5.RElern;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MoneyGame {
+    public static void main(String[] args) throws IOException {
+
+        int playerCoin = random();
+        int playerCredit = 0;
+
+        System.out.println("У вас на счету: "+ playerCredit +"$");
+        System.out.println("Хотите поиграть ?");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите 1 , если хотите сыграть ");
+        System.out.println("Введите 0 , если сегодня не ваш день ");
+        byte choice = HM8.byteOnly(reader);
+
+        gambleChoice(playerCoin, playerCredit, choice);
+
+
+    }
+
+    private static void gambleChoice(int playerCoin, int playerCredit, byte choice) {
+        switch (choice){
+            case 0:
+                System.out.println("Желаю вам удачи");
+            case 1:
+                gamble(playerCoin, playerCredit);
+            default:
+                System.out.println("Хорошего вечера");
+        }
+    }
+
+    private static int random() {
+        short max = 1000;
+        short min = 1;
+        int randomInRange = (int) ((Math.random()*(max - min))+min);
+        return randomInRange;
+    }
+
+    private static void gamble(int playerCoin, int playerCredit) {
+        if (playerCoin >= 1000){
+            playerCredit += 50000;
+        }else if (playerCoin >=500 && playerCoin <= 1000){
+            playerCredit += 20000;
+        }else if (playerCoin >= 100 && playerCoin <= 499){
+            playerCredit += 10000;
+        }
+        System.out.println("У вас на счету: "+ playerCredit +"$");
+    }
+}
