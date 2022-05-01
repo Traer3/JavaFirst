d6ae173d073a67c68a7383ab73149c71a7c29f69
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2022-05-01 14:22:19 +0300

    Update ArraySum.java

--- classwork/lesson8/ReLearnAgain/ArraySum.java
+++ classwork/lesson8/ReLearnAgain/ArraySum.java
@@ -4,11 +4,16 @@ public class ArraySum {
     public static void main(String[] args) {
 
         int[] numbers = {2, 3, 4, 5, 6};
+        int result = sumArray(numbers);
+        System.out.println(result);
+    }
+
+    public static int sumArray(int[] numbers) {
         int sumResult = 0;
         for (int i = 0; i < numbers.length; i++) {
             sumResult += numbers[i];
 
         }
-        System.out.println(sumResult);
+        return sumResult;
     }
 }
