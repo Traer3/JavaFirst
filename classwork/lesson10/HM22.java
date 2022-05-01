949aca3a333946fbb994944e0d56ebb8bf4f03d5
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2022-05-01 16:12:55 +0300

    Add files via upload

---
+++ classwork/lesson10/HM22.java
@@ -0,0 +1,13 @@
+package com.hillel.classwork.lesson10;
+
+import com.hillel.classwork.lesson8.ReLearnAgain.TryCatch;
+
+public class HM22 {
+    public static void main(String[] args) {
+
+        System.out.println("Введите число");
+        int factorialNumber = TryCatch.intOnly();
+
+        System.out.println(FactorialN.factorial(factorialNumber));
+    }
+}
